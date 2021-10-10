c903f5debd01e5f369559e10907e598202d69804
dburka <dburka001@gmail.com>
2021-10-10 18:09:22 +0200

    2/4

--- MintaZh1/MintaZh1/Form1.cs
+++ MintaZh1/MintaZh1/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +23,39 @@ namespace MintaZh1
         }
 
         private void LoadData(string fileName)
-        { 
+        {
+            using (var sr = new StreamReader(fileName, Encoding.Default))
+            {
+                sr.ReadLine(); // Az első sort eldobjuk
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(',');
+                    var or = new OlympicResult()
+                    {
+                        Year = int.Parse(line[0]),
+                        Country = line[3],
+                        Medals = new int[]
+                        {
+                            int.Parse(line[5]),
+                            int.Parse(line[6]),
+                            int.Parse(line[7])
+                        }
+                    };
+                    // Egy kevésbé kompakt, de talán átláthatóbb írásmód
+
+                    //var medals = new int[3];
+                    //medals[0] = int.Parse(line[5]);
+                    //medals[1] = int.Parse(line[6]);
+                    //medals[2] = int.Parse(line[7]);
+
+                    //var or = new OlympicResult();
+                    //or.Year = int.Parse(line[0]);
+                    //or.Country = line[3];
+                    //or.Medals = medals;                    
+
+                    results.Add(or);
+                }
+            }
         }
     }
 }
